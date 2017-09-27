ea763716d84cbda3ee98c0b3be7bb5831020a93a
ezirmusitua <jferroal@gmail.com>
2017-09-27 16:46:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 
 setup(
     name='getIpGeoInfo',
-    version='0.1.3',
+    version='0.1.4',
     description='Use geoip2 to get the ip address\'s geo info',
     packages=['getIpGeoInfo'],
     keywords=['ip', 'geoip2'],
     author='jferroal',
     author_email='jferroal@gmail.com',
-    url='https://github.com/ezirmusitua/proxies/tree/master/ipgeoinfo',
+    url='https://github.com/ezirmusitua/ipgeo',
+    download_url='https://github.com/ezirmusitua/ipgeo/releases',
     license='MIT'
 )
